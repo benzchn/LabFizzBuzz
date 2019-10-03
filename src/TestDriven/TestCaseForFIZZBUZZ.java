080ee9218e7d1aeaa51d8fe46ba6b4bf1865e4c6
benzchn <chanon.chapirom@gmail.com>
2019-10-03 16:47:05 +0700

    Create test input 3

--- src/TestDriven/TestCaseForFIZZBUZZ.java
+++ src/TestDriven/TestCaseForFIZZBUZZ.java
@@ -12,6 +12,7 @@ class TestCaseForFIZZBUZZ {
 		int input = fizzbuzz.printStringofInput(1);
 		assertEquals(1,input);
 	}
+	
 	@Test
 	void test_input_is_2_expect_output_is_2() {
 		FizzBuzz fizzbuzz = new FizzBuzz();
@@ -19,5 +20,11 @@ class TestCaseForFIZZBUZZ {
 		assertEquals(2,input);
 	}
 
+	@Test
+	void test_input_is_3_expect_output_is_Fizz() {
+		FizzBuzz fizzbuzz = new FizzBuzz();
+		int input = fizzbuzz.printStringofInputFizz(3);
+		assertEquals("Fizz",input);
+	}
 	
 }
