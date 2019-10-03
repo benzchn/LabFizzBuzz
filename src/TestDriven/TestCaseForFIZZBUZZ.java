cfeb832aa153bce2de7835b11d85b3738cae41aa
benzchn <chanon.chapirom@gmail.com>
2019-10-03 15:46:59 +0700

    Create FizzBuzz

---
+++ src/TestDriven/TestCaseForFIZZBUZZ.java
@@ -0,0 +1,16 @@
+package TestDriven;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestCaseForFIZZBUZZ {
+
+	@Test
+	void test_input_is_1_expect_output_is_1() {
+		FizzBuzz fizzbuzz = new FizzBuzz();
+		int input = fizzbuzz.printStringofInput();
+		assertEquals(1,input);
+	}
+
+}
